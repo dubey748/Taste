b71fda70ca3194bd248ad336565072506d060855
dubey748 <dubeyabhishek748@gmail.com>
2023-04-05 03:55:01 +0530

    https://www.youtube.com/watch?v=zQAdZYxbH14&list=PLI0saxAvhd_OdRWyprSe3Mln37H0u4DAp

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ 
+    <App />
+ 
+);
+
+
+reportWebVitals();
